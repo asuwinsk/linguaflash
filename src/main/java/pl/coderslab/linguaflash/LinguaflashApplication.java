c2bd6f651c95be3ee90ad2939db4643460db404e
asuwinsk <asuwinsk@visa.com>
2025-08-28 14:26:10 +0200

    Initial Spring Boot project

---
+++ src/main/java/pl/coderslab/linguaflash/LinguaflashApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.linguaflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaflashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinguaflashApplication.class, args);
+    }
+
+}
